2f68e37f9cdbdcab3ab87f9b3d7bc8c1d612bfd5
Oliver Morris <officialgamermorris@gmail.com>
2017-09-20 14:07:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 setup(
-    name='bitcoin-python',
+    name='Maxcoin-python',
     version='0.3',
-    description='Friendly Bitcoin JSON-RPC API binding for Python',
+    description='Friendly Maxcoin JSON-RPC API binding for Python',
     long_description='This package allows performing commands such as listing the current balance'
     ' and sending coins to the Satoshi (original) client from Python. The communication with the'
     ' client happens over JSON-RPC.',
-    maintainer='Wladimir van der Laan',
-    maintainer_email='laanwj@gmail.com',
-    url='http://laanwj.github.com/bitcoin-python/doc/',
+    maintainer='Ollie Morris',
+    maintainer_email='officialgamermorris@gmail.com',
+    url='https://github.com/Olliecad1/Maxcoin-python',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
